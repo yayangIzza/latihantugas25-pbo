de91fedc3a377e18deb0b429204a3828b79eda13
yayangIzza <149374422+yayangIzza@users.noreply.github.com>
2023-10-30 19:50:56 +0700

    Add files via upload

---
+++ ejaan_nama.php
@@ -0,0 +1,18 @@
+<?php
+echo "Nama  : Yayang Izzatul Haq<br>";
+echo "NIM   : 22205052<br>";
+echo "Kelas : PBO<br>";
+
+echo "masukkan nama depan anda untuk di eja : suratmi<br>";
+
+echo "ejaan untuk suratmi adalah:<br>";
+
+echo "Huruf ke-1 : s<br>";
+echo "Huruf ke-2 : u<br>";
+echo "Huruf ke-3 : r<br>";
+echo "Huruf ke-4 : a<br>";
+echo "Huruf ke-5 : t<br>";
+echo "Huruf ke-6 : m<br>";
+echo "Huruf ke-7 : i<br>";
+
+?>
